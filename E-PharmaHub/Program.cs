6305a8d794505e79c6506d276391adbbd5cc61c7
Yasmin123-design <https://github.com/Yasmin123-design/E-HealthHub.git>
2025-10-14 21:46:35 +0300

    addserviceindependecyinjection

--- E-PharmaHub/Program.cs
+++ E-PharmaHub/Program.cs
@@ -96,6 +96,9 @@ namespace E_PharmaHub
             builder.Services.AddScoped<IClinicService, ClinicService>();
             builder.Services.AddScoped<IDonorRepository, DonorRepository>();
             builder.Services.AddScoped<IDonorService, DonorService>();
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+
             builder.Services.AddScoped<IPharmacistRepository, PharmacistRepository>();
             builder.Services.AddScoped<IPharmacistService, PharmacistService>();
             builder.Services.AddScoped<IMedicineService, MedicineService>();
