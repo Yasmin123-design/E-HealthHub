d98274cb1628a07dbc9988a558a349e48e4d971c
Yasmin123-design <https://github.com/Yasmin123-design/E-HealthHub.git>
2025-10-16 18:52:10 +0300

    allowsignalrcors

--- E-PharmaHub/Program.cs
+++ E-PharmaHub/Program.cs
@@ -23,14 +23,16 @@ namespace E_PharmaHub
 
             builder.Services.AddCors(options =>
             {
-                options.AddPolicy("AllowAll",
-                    policy =>
-                    {
-                        policy.AllowAnyOrigin()   
-                              .AllowAnyHeader()  
-                              .AllowAnyMethod(); 
-                    });
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials() 
+                        .SetIsOriginAllowed(_ => true); 
+                });
             });
+
 
             builder.Services.AddControllers()
                          .AddJsonOptions(options =>
