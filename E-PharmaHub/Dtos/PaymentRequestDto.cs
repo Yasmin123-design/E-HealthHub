cbd1fec5b080ec4b64fb07e5f3765fab5f93bdb5
Yasmin123-design <https://github.com/Yasmin123-design/E-HealthHub.git>
2025-10-18 07:29:27 +0300

    amountallownullinpaymentdto

--- E-PharmaHub/Dtos/PaymentRequestDto.cs
+++ E-PharmaHub/Dtos/PaymentRequestDto.cs
@@ -8,7 +8,7 @@ namespace E_PharmaHub.Dtos
         public int? PharmacistId { get; set; }
         public string? ReferenceId { get; set; }
         public PaymentForType PaymentFor { get; set; }
-        public decimal Amount { get; set; }
+        public decimal? Amount { get; set; }
         public int? OrderId { get; set; }
         public int? AppointmentId { get; set; }
 
